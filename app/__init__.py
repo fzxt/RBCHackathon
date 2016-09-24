9d9a5bd95f14da9ac6884973763ef98dbe22021b
Igor Laskaev <i.laskaev@gmail.com>
2016-09-24 16:31:49 -0400

    Graphing complete

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,9 @@
 from flask import Flask, render_template, jsonify
-from faker import Factory
-from datetime import datetime
-import random
-import operator
+from app.helpers.helper import get_all_data
 
 public = 'public'
 
 app = Flask(__name__, template_folder=public, static_folder=public)
-
-faker = Factory.create()
 
 
 @app.route('/')
@@ -16,70 +11,25 @@ def index():
   return render_template('index.html')
 
 
+@app.route('/visual')
+def visual():
+  return render_template('visual.html')
+
+
+# Oh god...
 @app.route('/api/transactions')
-def get_all_data():
-  entries = []
-
-  rent = {
-    'time': datetime(
-      year=2016,
-      month=9,
-      day=1
-    ),
-    'name': 'Toronto Leasing',
-    'notes': 'Business Rent',
-    'amount': float(-3000.00),
-    'type': 'C'
-  }
-  entries.append(rent)
-
-  for num in range(0, 6):
-    salaries = {
-      'time': datetime(
-        year=2016,
-        month=9,
-        day=1
-      ),
-      'name': 'Interac e-Transfer',
-      'notes': 'Employee Salary',
-      'amount': float(-2080.00),
-      'type': 'C'
-    }
-    entries.append(salaries)
-
-  for num in range(0, 50):
-    year = 2016
-    month = 9
-    day = random.randint(1, 30)
-    hour = random.randint(0, 23)
-    minute = random.randint(0, 59)
-    second = random.randint(0, 59)
-    date = datetime(
-      year=year,
-      month=month,
-      day=day,
-      hour=hour,
-      minute=minute,
-      second=second
-    )
-
-    income = float("{0:.2f}".format(random.uniform(-500, 1000)))
-    data = {
-      'time': date,
-      'name': faker.company(),
-      'notes': '--',
-      'amount': income,
-      'type': 'D' if income > 0 else 'C'
-    }
-    entries.append(data)
-  entries.sort(key=operator.itemgetter('time'))
-
-  total = 0
-  for entry in entries:
-    total += entry['amount']
-
-
-  return jsonify({
-    'total': total,
-    'data': entries}
-  )
+def test():
+  data = get_all_data()
+  times = {}
+  for entry in data:
+    try:
+      a = times[entry['date']]
+      a['total'] += entry['amount']
+      a['transactions'] = a['transactions'] + 1
+    except:
+      times[entry['date']] = {
+        'total': entry['amount'],
+        'date': entry['date'],
+        'transactions': 1
+      }
+  return jsonify(times)
---
+++ app/helpers/helper.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+import random
+import operator
+from faker import Factory
+
+def get_all_data():
+  faker = Factory.create()
+
+  entries = []
+
+  rent = {
+    'date': datetime(
+      year=2016,
+      month=9,
+      day=1
+    ),
+    'name': 'Toronto Leasing',
+    'notes': 'Business Rent',
+    # 'amount': float(-3000.00),
+    'amount': -3000,
+    'type': 'C'
+  }
+  entries.append(rent)
+
+  for num in range(1, 4):
+    salaries = {
+      'date': datetime(
+        year=2016,
+        month=9,
+        day=1
+      ),
+      'name': 'Interac e-Transfer',
+      'notes': 'Employee Salary',
+      'amount': -2080,
+      'type': 'C'
+    }
+    entries.append(salaries)
+
+  for num in range(0, 610):
+    year = 2016
+    month = 9
+    day = random.randint(1, 30)
+    hour = random.randint(0, 23)
+    minute = random.randint(0, 59)
+    second = random.randint(0, 59)
+    date = datetime(
+      year=year,
+      month=month,
+      day=day,
+      hour=hour,
+      minute=minute,
+      second=second
+    )
+
+    income = random.randint(-500, 500)
+    data = {
+      'date': date,
+      'name': faker.company(),
+      'notes': '--',
+      'amount': income,
+      'type': 'D' if income > 0 else 'C'
+    }
+    entries.append(data)
+  entries.sort(key=operator.itemgetter('date'))
+
+  total = 0
+  for entry in entries:
+    entry['date'] = entry['date'].strftime("%d-%m-%y")
+    total += entry['amount']
+
+  return entries
