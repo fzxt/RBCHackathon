1fdd8d07c31ef485ef89d7235b383a9da8c92d0f
Adam Balint <ab11fl@brocku.ca>
2016-09-24 12:42:45 -0400

    Forced decimals to 2 digits

--- GenCustomerData.py
+++ GenCustomerData.py
@@ -47,7 +47,7 @@ for month in range(0,len(numDays)):
             for expense in c1:
                 if (expense[1] == "Rent"):
                     f.write(str(year) +"-"+str(month+1).zfill(2)+"-"+str(day).zfill(2)+",")
-                    f.write("-"+str(expense[2]) + "," + "Landlord,Rent\n")
+                    f.write("-"+str("%0.2f" % expense[2]) + "," + "Landlord,Rent\n")
 
         if ((day == payEmp[0]) | (day == payEmp[1])):
             print ("Paying employees")
@@ -56,19 +56,19 @@ for month in range(0,len(numDays)):
                     if (expense[1] == "Employee"):
                         print ("Paying employee " + str(i))
                         f.write(str(year) +"-"+str(month+1).zfill(2)+"-"+str(day).zfill(2)+",")
-                        f.write("-"+str(expense[2]) + "," + "Employee,Paycheck\n")
+                        f.write("-"+str("%0.2f" % expense[2]) + "," + "Employee,Paycheck\n")
         for i in range(len(c1)):
             expense = c1[i]
             if (expense[0] != 0 & randint(0,100) < 10):
                 current_cost = float(expense[2])*float(randint(1,50)/100)
                 total = float(current_cost) + (float(current_cost) * ((randint(-expense[3], expense[3]))/100))
                 f.write(str(year) +"-"+str(month+1).zfill(2)+"-"+str(day).zfill(2)+",")
-                f.write("-"+str(total) + "," + "Payable," + expense[1] + " \n")
+                f.write("-"+str("%0.2f" % total) + "," + "Payable," + expense[1] + " \n")
         cust = randint(1,50)
         for i in range(1,cust):
             spent = random.uniform(5, 200)
             f.write(str(year) +"-"+str(month+1).zfill(2)+"-"+str(day).zfill(2)+",")
-            f.write(str(spent) + "," + "Customer," + "Purchase" + " \n")
+            f.write(str("%0.2f" % spent) + "," + "Customer," + "Purchase" + " \n")
 
 
 f.close()
