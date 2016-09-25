cb03d91a30a7e09034b7fda7a4b1ff92c8857e16
Igor Laskaev <i.laskaev@gmail.com>
2016-09-25 00:33:48 -0400

    Page 2 done

--- app/public/app/loans/loanController.js
+++ app/public/app/loans/loanController.js
@@ -1,3 +1,14 @@
 app.controller('LoanController', function($scope) {
-  $scope.test = "TEST SCOPE";
+  $scope.loan = 2000;
+  $scope.price = {
+    one: 200,
+    two: 400,
+    three: 750,
+    four: 1000
+  }
+
+  $scope.isPriceValid = function(price){
+    return price > 0 && price <= $scope.loan ? 'green' : 'red';
+  }
+
 })
